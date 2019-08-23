233e7f82c6b4bea3ab8c1f5ad47f8b7bded58519
yidahc <46684324+yidahc@users.noreply.github.com>
2019-08-22 20:02:10 -0500

    finished HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+/*
+(1) Write a program that outputs the following.
+Hello world!
+
+(2) Update to output the following.
+Hello world!
+How are you? 
+
+(3) Finally, update to output the following. End with a newline. 
+Hello world!
+How are you?
+   (I'm fine).
+*/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+       
+       System.out.println("Hello world!");
+       System.out.println("How are you?");
+       System.out.println("   (I'm fine).");
+       
+       return;
+    }
+ }
